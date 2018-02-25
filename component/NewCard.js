beb0fc6409d462b6a6eab023cba8437626f091c8
Syed Aman <sna274@nyu.edu>
2018-02-25 17:08:55 -0600

    create new card page

---
+++ component/NewCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const NewCard = () => (
+	<View>
+		<View>
+			<Text>Question</Text>
+			<Text>What is the color of the sky?</Text>
+		</View>
+		<View>
+			<Text>Answer</Text>
+			<Text>Sky Blue</Text>
+		</View>
+	</View>
+);
+
+export default NewCard;
