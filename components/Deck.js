461c27841cfe908b1af507ed09632ec2e0c6202a
Syed Aman <sna274@nyu.edu>
2018-02-25 17:30:10 -0600

    rename component directory to components

---
+++ components/Deck.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+const Deck = () => (
+	<View>
+		<Text>Colors</Text>
+		<Text>5 Cards</Text>
+
+		<Button title="Add Card" />
+		<Button title="Start Quiz" />
+	</View>
+);
+
+export default Deck;
