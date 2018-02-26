184518bf0d1793af09f61d05ff2cf53f2b4c6c65
Syed Aman <sna274@nyu.edu>
2018-02-25 18:10:39 -0600

    render the stack navigator

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Home from './components/Home.js';
+import MainNavigator from './components/MainNavigator';
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Home />
+				<MainNavigator />
 			</View>
 		);
 	}
