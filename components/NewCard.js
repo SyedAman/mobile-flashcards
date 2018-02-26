0b4867af69ba89614f661ae5de53b004f5c2aae5
Syed Aman <sna274@nyu.edu>
2018-02-25 20:56:20 -0600

    refactor newcard page

--- components/NewCard.js
+++ components/NewCard.js
@@ -3,12 +3,8 @@ import { View, Text, TextInput } from 'react-native';
 
 const NewCard = () => (
 	<View>
-		<View>
-			<TextInput placeholder="Question" />
-		</View>
-		<View>
-			<TextInput placeholder="Correct Answer" />
-		</View>
+		<TextInput placeholder="Question" />
+		<TextInput placeholder="Correct Answer" />
 	</View>
 );
 
