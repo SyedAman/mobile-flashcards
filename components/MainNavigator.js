20d79ab995fcf325a0aedb5965d58b76ffec0528
Syed Aman <sna274@nyu.edu>
2018-02-25 19:55:21 -0600

    add in more screens to the stacknavigator

--- components/MainNavigator.js
+++ components/MainNavigator.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { StackNavigator } from 'react-navigation';
 
 import Home from './Home';
+import Deck from './Deck';
+import NewCard from './NewCard';
+import Quiz from './Quiz';
 
 const MainNavigator = StackNavigator(
 	{
 		HomePage: {
 			screen: Home,
+		},
+		IndividualDeckPage: {
+			screen: Deck,
+		},
+		NewCardPage: {
+			screen: NewCard,
+		},
+		Quiz: {
+			screen: Quiz,
 		},
 	},
 	{
