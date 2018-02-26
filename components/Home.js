49ce88c8e6259777165defe2e943bec11b3f53bb
Syed Aman <sna274@nyu.edu>
2018-02-25 18:54:59 -0600

    render the MainNavigator properly displaying the home page as the initial route

--- components/Home.js
+++ components/Home.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
 
 const Home = () => (
-	<View>
+	<View style={styles.container}>
 		<Text>Home Page</Text>
 	</View>
 );
--- components/MainNavigator.js
+++ components/MainNavigator.js
@@ -3,10 +3,16 @@ import { StackNavigator } from 'react-navigation';
 
 import Home from './Home';
 
-const MainNavigator = StackNavigator({
-	Home: {
-		screen: Home,
+const MainNavigator = StackNavigator(
+	{
+		HomePage: {
+			screen: Home,
+		},
 	},
-});
+	{
+		initialRouteName: 'HomePage',
+		headerMode: 'none',
+	},
+);
 
 export default MainNavigator;
