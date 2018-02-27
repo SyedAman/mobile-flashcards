a4eb68f2871678bf34cde8d508d4d25e44a14048
Syed Aman <saman@elevate.com>
2018-02-27 04:46:35 -0600

    add cardsjs

---
+++ reducers/cards.js
@@ -0,0 +1,10 @@
+import initialState from './initialState';
+import actionTypes from '../actions/actionTypes';
+
+function cards(state = initialState.cards, action) {
+    switch(action.type) {
+        case:
+    }
+}
+
+export default cards;
