cfc253cb70c625f7f9ecc947fe238ff3109e02a7
Syed Aman <sna274@nyu.edu>
2018-02-27 08:03:48 -0600

    create an action creator for creating decks

---
+++ actions/index.js
@@ -0,0 +1,10 @@
+import uniqid from 'uniqid';
+
+import * as actionTypes from './actionTypes';
+
+export const createDeck = ({ name }) => ({
+	type: actionTypes.CREATE_DECK,
+	id: uniqid(),
+	name,
+	cardsById: [],
+});
