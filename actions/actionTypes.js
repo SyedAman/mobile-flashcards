24f4e1312077613ff37c7f9159c054dcd806c1da
Syed Aman <sna274@nyu.edu>
2018-02-27 08:00:17 -0600

    add more actions

--- actions/actionTypes.js
+++ actions/actionTypes.js
@@ -1,2 +1,4 @@
-export const ADD_CARD_TO_DECK = "ADD_CARD_TO_DECK";
-export const CREATE_DECK = "CREATE_DECK";
+export const ADD_CARD_TO_DECK = 'ADD_CARD_TO_DECK';
+export const CREATE_DECK = 'CREATE_DECK';
+export const ADD_QUESTION_TO_CARD = 'ADD_QUESTION_TO_CARD';
+export const ADD_ANSWER_TO_CARD = 'ADD_ANSWER_TO_CARD';
