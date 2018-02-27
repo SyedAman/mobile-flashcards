3d4b5085e5aa8f6a9e8da19b709e496f31bf1be9
Syed Aman <sna274@nyu.edu>
2018-02-27 06:46:54 -0600

    update initial state for reducers

--- reducers/initialState.js
+++ reducers/initialState.js
@@ -1,36 +1,40 @@
 const initialState = {
-	decks: [
-		{
-			name: 'colors',
-			cards: [
-				{
-					question: 'what is the color of the sky?',
-					answer: 'blue',
-				},
-				{
-					question: 'what is the color of lava?',
-					answer: 'orange',
-				},
-				{
-					question: 'what is the color of cloraphyll?',
-					answer: 'green',
-				},
-			],
+	decks: {
+		byId: {
+			jasd123fas: {
+				name: 'colors',
+				cardsById: ['asd213', 'ggajkj123', 'oopuioasd323'],
+			},
+			gascvm213: {
+				name: 'food',
+				cardsById: ['asdasdpopo213', 'aczxnmn23'],
+			},
 		},
-		{
-			name: 'food',
-			cards: [
-				{
-					question: 'what is everyones food?',
-					answer: 'pizza',
-				},
-				{
-					question: 'what is the black syrup on sundaes?',
-					answer: 'fudge',
-				},
-			],
+	},
+	cards: {
+		byId: {
+			asd213: {
+				question: 'what is the color of the sky?',
+				answer: 'blue',
+			},
+			ggajkj123: {
+				question: 'what is the color of lava?',
+				answer: 'orange',
+			},
+			oopuioasd323: {
+				question: 'what is the color of cloraphyll?',
+				answer: 'green',
+			},
+			asdasdpopo213: {
+				question: 'what is everyones food?',
+				answer: 'pizza',
+			},
+			aczxnmn23: {
+				question: 'what is the black syrup on sundaes?',
+				answer: 'fudge',
+			},
 		},
-	],
+	},
 };
 
 export default initialState;
