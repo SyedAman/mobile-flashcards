15f878aa119d80ca69de103510a24fd4e42c000c
Syed Aman <sna274@nyu.edu>
2018-02-27 19:35:10 -0600

    add in thunk middleware and the logging middleware into the store

--- store/index.js
+++ store/index.js
@@ -1,5 +1,19 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 
-import allReducers from '../reducers';
+import rootReducer from '../reducers';
 
-export default (store = createStore(allReducers));
+const composeEnhancersWithReduxDevTools =
+	window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+	rootReducer,
+	composeEnhancersWithReduxDevTools(
+		applyMiddleware(thunkMiddleware, loggerMiddleware),
+	),
+);
+
+export default store;
