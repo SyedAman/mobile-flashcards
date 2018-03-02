06cd47b3a9e676c3eee225106ff6df4c7b50778a
Syed Aman <sna274@nyu.edu>
2018-03-01 22:50:27 -0600

    add in functionality to create a deck in the create new deck page

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import uniqid from 'uniqid';
 
 import * as actionTypes from './actionTypes';
 
-export const createDeck = ({ name }) => ({
+export const createDeck = name => ({
 	type: actionTypes.CREATE_DECK,
 	id: uniqid(),
 	name,
--- src/components/CreateNewDeck.js
+++ src/components/CreateNewDeck.js
@@ -1,10 +1,59 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import styled from 'styled-components';
+import { connect } from 'react-redux';
 
-const CreateNewDeck = () => (
-	<View>
-		<Text>New Deck</Text>
-	</View>
-);
+import * as actionCreators from '../actions';
 
-export default CreateNewDeck;
+const Container = styled.View`
+	flex: 1;
+	align-items: center;
+	justify-content: center;
+`;
+
+const Header = styled.Text``;
+
+const SubmitButton = styled.TouchableOpacity`
+	border-radius: 25px;
+	width: 100px;
+	height: 100px;
+`;
+
+const NewDeckNameTextField = styled.TextInput``;
+
+class CreateNewDeck extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			newDeckName: '',
+		};
+	}
+
+	handleSubmit = newDeckName => {
+		this.props.createDeck(newDeckName);
+		this.props.navigation.navigate('HomePage');
+	};
+
+	render() {
+		return (
+			<Container>
+				<Header>New Deck</Header>
+
+				<NewDeckNameTextField
+					onChangeText={newText => this.setState({ newDeckName: newText })}
+				/>
+
+				<SubmitButton onPress={() => handleSubmit(this.state.newDeckname)}>
+					<Text>Submit</Text>
+				</SubmitButton>
+			</Container>
+		);
+	}
+}
+
+const mapDispatchToProps = dispatch => ({
+	createDeck: newDeckName => dispatch(actionCreators.createDeck(newDeckName)),
+});
+
+export default connect(mapDispatchToProps)(CreateNewDeck);
