3725f9765f4b81265adc594213b0f683706102d6
Syed Aman <sna274@nyu.edu>
2018-03-01 21:21:11 -0600

    implement redux debugger

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,15 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'remote-redux-devtools';
 
 import rootReducer from '../reducers';
-
-const composeEnhancersWithReduxDevTools =
-	window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const loggerMiddleware = createLogger();
 
 const store = createStore(
 	rootReducer,
-	composeEnhancersWithReduxDevTools(
-		applyMiddleware(thunkMiddleware, loggerMiddleware),
-	),
+	composeWithDevTools(applyMiddleware(thunkMiddleware, loggerMiddleware)),
 );
 
 export default store;
