29364f094923f6778c78dbae9940b46abf67a818
Syed Aman <sna274@nyu.edu>
2018-03-01 21:43:21 -0600

    create a reducer switch case

--- src/reducers/cards.js
+++ src/reducers/cards.js
@@ -4,10 +4,8 @@ import * as actionTypes from '../actions/actionTypes';
 export default function cards(previousState = initialState.cards, action) {
 	switch (action.type) {
 		case actionTypes.ADD_QUESTION_TO_CARD:
-			console.log(action);
 			return previousState;
 		case actionTypes.ADD_ANSWER_TO_CARD:
-			console.log(action);
 			return previousState;
 		default:
 			return previousState;
--- src/reducers/decks.js
+++ src/reducers/decks.js
@@ -5,8 +5,18 @@ import * as actionTypes from '../actions/actionTypes';
 function decks(previousState = initialState.decks, action) {
 	switch (action.type) {
 		case actionTypes.CREATE_DECK:
-			console.log(action);
-			return previousState;
+			let { id, name, cardsById } = action;
+			return {
+				...previousState,
+				byId: {
+					...previousState.byId,
+					[id]: {
+						id,
+						name,
+						cardsById,
+					},
+				},
+			};
 		default:
 			return previousState;
 	}
