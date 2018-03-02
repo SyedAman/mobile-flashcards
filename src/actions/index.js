7edf44535e3ea875e0d6e6695098e174e954b597
Syed Aman <sna274@nyu.edu>
2018-03-02 07:03:12 -0600

    remove uniqid since that isn't compatible with react native's expo environment

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,17 @@
-import uniqid from 'uniqid';
+import uuid from 'react-native-uuid';
 
 import * as actionTypes from './actionTypes';
 
 export const createDeck = name => ({
 	type: actionTypes.CREATE_DECK,
-	id: uniqid(),
+	id: uuid.v1(),
 	name,
 	cardsById: [],
 });
 
 export const addCardToDeck = ({ parentId, question, answer }) => ({
 	type: actionTypes.CREATE_DECK,
-	id: uniqid(),
+	id: uuid.v1(),
 	parentId,
 	question,
 	answer,
