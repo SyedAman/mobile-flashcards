b37be2135c5aee293dd41e9f250b6efe03d76bde
Syed Aman <sna274@nyu.edu>
2018-03-03 08:40:46 -0600

    prettify actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,16 +3,16 @@ import uuid from 'react-native-uuid';
 import * as actionTypes from './actionTypes';
 
 export const createDeck = name => ({
-	type: actionTypes.CREATE_DECK,
-	id: uuid.v1(),
-	name,
-	cardsById: [],
+  type: actionTypes.CREATE_DECK,
+  id: uuid.v1(),
+  name,
+  cardsById: [],
 });
 
-export const addCardToDeck = ({ parentId, question, answer }) => ({
-	type: actionTypes.CREATE_DECK,
-	id: uuid.v1(),
-	parentId,
-	question,
-	answer,
+export const addCardToDeck = ({parentId, question, answer}) => ({
+  type: actionTypes.CREATE_DECK,
+  id: uuid.v1(),
+  parentId,
+  question,
+  answer,
 });
