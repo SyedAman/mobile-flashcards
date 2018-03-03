d39ecefaddf437c1715d2c9aec8a3602601a9195
Syed Aman <sna274@nyu.edu>
2018-03-03 08:17:13 -0600

    prettify app.js

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 
 import MainNavigator from './src/components/MainNavigator';
 import store from './src/store';
 
 export default () => (
-	<Provider store={store}>
-		<MainNavigator />
-	</Provider>
+  <Provider store={store}>
+    <MainNavigator />
+  </Provider>
 );
