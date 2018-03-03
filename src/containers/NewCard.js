5a2a4271457fcfad4e3c48dbe2e1870449b1ee35
Syed Aman <sna274@nyu.edu>
2018-03-03 08:20:18 -0600

    prettify newcard component

--- src/containers/NewCard.js
+++ src/containers/NewCard.js
@@ -1,48 +1,48 @@
-import React, { Component } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import {connect} from 'react-redux';
 
-import { addCardToDeck } from '../actions';
+import {addCardToDeck} from '../actions';
 
 class NewCard extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			cardQuestion: '',
-			cardAnswer: '',
-		};
-	}
+    this.state = {
+      cardQuestion: '',
+      cardAnswer: '',
+    };
+  }
 
-	handleSubmit = () => {
-		const newCardData = {
-			parentId: '',
-			question: this.state.cardQuestion,
-			answers: this.state.cardAnswer,
-		};
+  handleSubmit = () => {
+    const newCardData = {
+      parentId: '',
+      question: this.state.cardQuestion,
+      answers: this.state.cardAnswer,
+    };
 
-		this.props.onAddCardToDeck(newCardData);
-	};
+    this.props.onAddCardToDeck(newCardData);
+  };
 
-	render() {
-		return (
-			<View>
-				<TextInput
-					onTextChange={newText => this.setState({ cardQuestion: newText })}
-					placeholder="Question"
-				/>
-				<TextInput
-					onTextChange={newText => this.setState({ cardAnswer: newText })}
-					placeholder="Correct Answer"
-				/>
-				<TouchableOpacity onPress={this.handleSubmit} />
-			</View>
-		);
-	}
+  render() {
+    return (
+      <View>
+        <TextInput
+          onTextChange={newText => this.setState({cardQuestion: newText})}
+          placeholder="Question"
+        />
+        <TextInput
+          onTextChange={newText => this.setState({cardAnswer: newText})}
+          placeholder="Correct Answer"
+        />
+        <TouchableOpacity onPress={this.handleSubmit} />
+      </View>
+    );
+  }
 }
 
 const mapDispatchToProps = dispatch => ({
-	onAddCardToDeck: newCard => dispatch(addCardToDeck(newCard)),
+  onAddCardToDeck: newCard => dispatch(addCardToDeck(newCard)),
 });
 
 export default connect(null, mapDispatchToProps)(NewCard);
