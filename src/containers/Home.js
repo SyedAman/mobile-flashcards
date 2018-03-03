7ab7c29a250a852178858307cd8442ea777a8c62
Syed Aman <sna274@nyu.edu>
2018-03-03 08:20:54 -0600

    prettify Home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,68 +1,66 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {connect} from 'react-redux';
 import styled from 'styled-components';
 
 import initialState from '../reducers/initialState';
 
 const Container = styled.View`
-	flex: 1;
-	background-color: #fff;
-	align-items: center;
-	justify-content: center;
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
 `;
 
 const DeckButton = styled(TouchableOpacity)`
-	background-color: rgb(139, 164, 201);
-	margin: 10px;
-	justify-content: space-around;
-	flex-direction: column;
+  background-color: rgb(139, 164, 201);
+  margin: 10px;
+  justify-content: space-around;
+  flex-direction: column;
 `;
 
 const CreateNewDeckButton = styled(TouchableOpacity)`
-	background-color: rgb(39, 200, 89);
-	width: 100px;
-	border-radius: 50px;
+  background-color: rgb(39, 200, 89);
+  width: 100px;
+  border-radius: 50px;
 `;
 
 class Home extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {};
-	}
+    this.state = {};
+  }
 
-	render() {
-		const { navigation, decks } = this.props;
+  render() {
+    const {navigation, decks} = this.props;
 
-		return (
-			<Container>
-				<CreateNewDeckButton
-					onPress={() => navigation.navigate('CreateNewDeckPage')}
-				>
-					<Text>+</Text>
-				</CreateNewDeckButton>
+    return (
+      <Container>
+        <CreateNewDeckButton
+          onPress={() => navigation.navigate('CreateNewDeckPage')}>
+          <Text>+</Text>
+        </CreateNewDeckButton>
 
-				<Text>NashCards</Text>
-				<Text>Decks</Text>
+        <Text>NashCards</Text>
+        <Text>Decks</Text>
 
-				{decks.map(deck => (
-					<DeckButton
-						onPress={() => navigation.navigate('IndividualDeckPage', { deck })}
-						title={deck.name}
-						key={deck.id}
-					>
-						<Text>{deck.name}</Text>
-						<Text>{`${deck.cardsById.length} cards`}</Text>
-					</DeckButton>
-				))}
-			</Container>
-		);
-	}
+        {decks.map(deck => (
+          <DeckButton
+            onPress={() => navigation.navigate('IndividualDeckPage', {deck})}
+            title={deck.name}
+            key={deck.id}>
+            <Text>{deck.name}</Text>
+            <Text>{`${deck.cardsById.length} cards`}</Text>
+          </DeckButton>
+        ))}
+      </Container>
+    );
+  }
 }
 
-const mapStateToProps = ({ decks, cards }) => ({
-	decks: Object.keys(decks.byId).map(deckId => decks.byId[deckId]),
+const mapStateToProps = ({decks, cards}) => ({
+  decks: Object.keys(decks.byId).map(deckId => decks.byId[deckId]),
 });
 
 export default connect(mapStateToProps)(Home);
