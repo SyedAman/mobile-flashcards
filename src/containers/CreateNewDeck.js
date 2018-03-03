06be38a65c0b1385d51e2cec6ccf272ed165078c
Syed Aman <sna274@nyu.edu>
2018-03-03 08:29:05 -0600

    prettify createnewdeck

--- src/containers/CreateNewDeck.js
+++ src/containers/CreateNewDeck.js
@@ -1,66 +1,66 @@
-import React, { Component } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
 import styled from 'styled-components';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
-import { createDeck } from '../actions';
+import {createDeck} from '../actions';
 
 const Container = styled.View`
-	flex: 1;
-	align-items: center;
-	justify-content: center;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Header = styled.Text``;
 
 const SubmitButton = styled.TouchableOpacity`
-	border-radius: 25px;
-	width: 100px;
-	height: 100px;
+  border-radius: 25px;
+  width: 100px;
+  height: 100px;
 `;
 
 const NewDeckNameTextField = styled.TextInput``;
 
 class CreateNewDeck extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			newDeckName: '',
-		};
-	}
+    this.state = {
+      newDeckName: '',
+    };
+  }
 
-	handleNewDeckNameInput = input => {
-		this.setState({ newDeckName: input });
-	};
+  handleNewDeckNameInput = input => {
+    this.setState({newDeckName: input});
+  };
 
-	handleSubmit = () => {
-		this.props.onSubmit(this.state.newDeckName);
-		this.props.navigation.navigate('HomePage');
-	};
+  handleSubmit = () => {
+    this.props.onSubmit(this.state.newDeckName);
+    this.props.navigation.navigate('HomePage');
+  };
 
-	render() {
-		return (
-			<Container>
-				<Header>What do you want to call your new deck?</Header>
+  render() {
+    return (
+      <Container>
+        <Header>What do you want to call your new deck?</Header>
 
-				<NewDeckNameTextField
-					onChangeText={newText => this.handleNewDeckNameInput(newText)}
-					placeholder="New deck name..."
-				/>
+        <NewDeckNameTextField
+          onChangeText={newText => this.handleNewDeckNameInput(newText)}
+          placeholder="New deck name..."
+        />
 
-				<SubmitButton onPress={() => this.handleSubmit()}>
-					<Text>Submit</Text>
-				</SubmitButton>
-			</Container>
-		);
-	}
+        <SubmitButton onPress={() => this.handleSubmit()}>
+          <Text>Submit</Text>
+        </SubmitButton>
+      </Container>
+    );
+  }
 }
 
 const mapDispatchToProps = dispatch => ({
-	onSubmit: newDeckName => {
-		dispatch(createDeck(newDeckName));
-	},
+  onSubmit: newDeckName => {
+    dispatch(createDeck(newDeckName));
+  },
 });
 
 export default connect(null, mapDispatchToProps)(CreateNewDeck);
