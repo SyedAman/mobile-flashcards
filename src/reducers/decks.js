69df577afc6a60dcf7cbcb0aee0f4cbf7ce54724
Syed Aman <sna274@nyu.edu>
2018-03-03 14:40:02 -0600

    fix issue where variables weren't able to be defined in switch cases

--- src/reducers/decks.js
+++ src/reducers/decks.js
@@ -3,39 +3,38 @@ import initialState from './initialState';
 import * as actionTypes from '../actions/actionTypes';
 
 function decks(previousState = initialState.decks, action) {
-	switch (action.type) {
-		case actionTypes.CREATE_DECK:
-			let { id, name, cardsById } = action;
-			return {
-				...previousState,
-				byId: {
-					...previousState.byId,
-					[id]: {
-						id,
-						name,
-						cardsById,
-					},
-				},
-			};
-    case actionTypes.ADD_CARD_TO_DECK:
-      let { id, parentId, question, answer } = action;
-      
+  switch (action.type) {
+    case actionTypes.CREATE_DECK: {
+      const {id, name, cardsById} = action;
+      return {
+        ...previousState,
+        byId: {
+          ...previousState.byId,
+          [id]: {
+            id,
+            name,
+            cardsById,
+          },
+        },
+      };
+    }
+    case actionTypes.ADD_CARD_TO_DECK: {
+      const {id, parentId, question, answer} = action;
+
       return {
         ...previousState,
         byId: {
           ...previousState.byId,
           [parentId]: {
             ...previousState.byId[parentId],
-            cardsById: [
-              ...previousState.byId[parentId].cardsById,
-              id
-            ]
-          }
-        }
-      }
-		default:
-			return previousState;
-	}
+            cardsById: [...previousState.byId[parentId].cardsById, id],
+          },
+        },
+      };
+    }
+    default:
+      return previousState;
+  }
 }
 
 export default decks;
