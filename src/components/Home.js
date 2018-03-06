09ff783d2d88ab6679f6495eddb8d6c5c8dee233
Syed Aman <sna274@nyu.edu>
2018-03-06 07:09:35 -0600

    remove constructor when its only calling super

--- src/components/Home.js
+++ src/components/Home.js
@@ -28,10 +28,6 @@ const NewDeckButton = GenericButton.extend`
 `;
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     const {navigation, decks} = this.props;
 
