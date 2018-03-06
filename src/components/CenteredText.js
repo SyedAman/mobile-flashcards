d6cfc7fd5263699c76fced38b02b1f5a9f93ee7b
Syed Aman <sna274@nyu.edu>
2018-03-06 06:36:32 -0600

    create centered text component

---
+++ src/components/CenteredText.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text} from 'react-native';
+import styled from 'styled-components';
+
+const CenteredText = styled.Text`
+  text-align: center;
+`;
+
+export default CenteredText;
